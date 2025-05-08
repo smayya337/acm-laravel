3ad9bb7c0d57b49735a1a2bd08f5d1d5b11ab746
Shreyas Mayya <shreyas@smayya.me>
2025-05-08 09:38:21 -0400

    make the login page look nicer

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,33 +2,37 @@
 
 @section('content')
     <h1>Log In</h1>
-    <form method="POST" action="{{ route('login') }}">
+    @error('username')
+    <div role="alert" class="alert alert-error mb-8">
+        <i class="fa-solid fa-circle-xmark"></i>
+        <span>{{ $message }}</span>
+    </div>
+    @enderror
+    @error('password')
+    <div role="alert" class="alert alert-error mb-8">
+        <i class="fa-solid fa-circle-xmark"></i>
+        <span>{{ $message }}</span>
+    </div>
+    @enderror
+    <form method="POST" action="{{ route('login') }}" class="space-y-4">
         @csrf
         @if (session('status'))
             <div>{{ session('status') }}</div>
         @endif
+        <label class="floating-label">
+            <span>Username</span>
+            <input id="username" type="text" name="username" value="{{ old('username') }}" class="input" placeholder="Username" required autofocus />
+        </label>
+        <label class="floating-label">
+            <span>Password</span>
+            <input id="password" type="password" name="password" class="input" placeholder="Password" required>
+        </label>
         <div>
-            <label for="username">Username</label>
-            <input id="username" type="text" name="username" value="{{ old('username') }}" required autofocus>
-            @error('username')
-            <div>{{ $message }}</div>
-            @enderror
+            <label class="me-1">Remember me</label>
+            <input type="checkbox" class="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
         </div>
         <div>
-            <label for="password">Password</label>
-            <input id="password" type="password" name="password" required>
-            @error('password')
-            <div>{{ $message }}</div>
-            @enderror
-        </div>
-        <div>
-            <label>
-                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                Remember me
-            </label>
-        </div>
-        <div>
-            <button type="submit">Login</button>
+            <button type="submit" class="btn btn-primary">Log In</button>
         </div>
         {{--        <div>--}}
         {{--            <a href="{{ route('password.request') }}">Forgot your password?</a>--}}

a4ad1fd64216976ba067685f1c3114aeb322a34b
Shreyas Mayya <shreyas@smayya.me>
2025-05-23 16:34:00 -0400

    reorganize routes

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Middleware\EnsureUserIsAdmin;
 
+// Standard routes (non-interactive)
+
 Route::get('/', function () {
     return view('index');
 })->name('home');
@@ -15,8 +17,6 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about');
 })->name('about');
-
-Route::get('/events', [EventController::class, 'index'])->name('events');
 
 Route::get('/icpc', function () {
     return view('icpc');
@@ -30,12 +30,7 @@ Route::get('/donate', function () {
     return view('donate');
 })->name('donate');
 
-Route::controller(UserController::class)->group(function () {
-    Route::get('/user/{user}', 'show')->middleware('can:view,user')->name('user_page');
-    Route::post('/user/{user}', 'update')->middleware('can:update,user')->name('user_page.update');
-});
-
-Route::get('/event/{event}', [EventController::class, 'show'])->middleware('can:view,event')->name('event_page');
+// Auth
 
 Route::get('/login', function () {
     return view('login');
@@ -46,11 +41,15 @@ Route::controller(AuthController::class)->group(function () {
     Route::get('/logout', 'logout')->name('logout');
 });
 
+// Admin interface
+
 Route::controller(AdminController::class)->group(function () {
     Route::get('/admin', 'index')->middleware(['auth', EnsureUserIsAdmin::class])->name('admin');
     Route::get('/admin/officers', 'officers')->middleware(['auth', EnsureUserIsAdmin::class])->name('admin.officers');
     Route::get('/admin/search/user', 'searchUsers')->middleware(['auth', EnsureUserIsAdmin::class])->name('admin.search.user');
 });
+
+// Officers (both admin and non-admin)
 
 Route::controller(OfficerController::class)->group(function () {
     Route::get('/officers', 'index')->name('officers');
@@ -59,4 +58,18 @@ Route::controller(OfficerController::class)->group(function () {
     Route::get('/admin/officers/{officer}', 'show')->middleware(['auth', EnsureUserIsAdmin::class])->name('admin.officers.show');
     Route::post('/admin/officers/{officer}', 'update')->middleware(['auth', EnsureUserIsAdmin::class])->name('admin.officers.update');
     Route::post('/admin/officers/{officer}/delete', 'destroy')->middleware(['auth', EnsureUserIsAdmin::class])->name('admin.officers.delete');
+});
+
+// Events (both admin and non-admin)
+
+Route::controller(EventController::class)->group(function () {
+    Route::get('/events', 'index')->name('events');
+    Route::get('/event/{event}', 'show')->middleware('can:view,event')->name('event_page');
+});
+
+// Users
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/user/{user}', 'show')->middleware('can:view,user')->name('user_page');
+    Route::post('/user/{user}', 'update')->middleware('can:update,user')->name('user_page.update');
 });
